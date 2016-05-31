9947dc95c4d21537f0bc8e396a56f47a5f5390dd
Javier Flores <junnyor.1011@gmail.com>
2016-05-31 13:15:14 -0500

    Pruebas unitarias

---
+++ TestEvo/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEvo
+{
+    public class Class1
+    {
+
+        public double factorial(int num)
+        {
+            int fac = 1;
+            while (num != 0)
+            {
+                fac = fac * num;
+                num--;
+            }
+            return fac;
+        }
+        public bool multiplo(int num, int mul)
+        {
+            if (num % mul == 0)
+                return true;
+            return false;
+        }
+        public bool comparar(int a, int b)
+        {
+            if (a < b)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ TestEvoTests/Class1Tests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestEvo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEvo.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void factorialTest()
+        {
+            Class1 prueba = new Class1();
+            double resultado = prueba.factorial(5);
+
+            //EXPECTATIVA, Valor de tu funcion
+            Assert.AreEqual(120, resultado);
+        }
+
+        [TestMethod()]
+        public void multiploTest()
+        {
+            Class1 prueba = new Class1();
+            bool resultado = prueba.multiplo(25, 2);
+            Assert.AreEqual(false, resultado);
+        }
+
+        [TestMethod()]
+        public void compararTest()
+        {
+            Class1 prueba = new Class1();
+            bool resultado = prueba.comparar(25, 2);
+            Assert.AreEqual(false, resultado);
+        }
+    }
+}
